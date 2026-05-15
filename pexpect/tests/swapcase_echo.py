4a082e76d22e81687f3da88a9efd6686db5e5d97
pponce <pponce@users.noreply.github.com>
2026-05-14 18:08:50 -0700

    Clarify .credentials username behavior for no-login RPC3

--- pexpect/tests/swapcase_echo.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env python
-import sys, time
-while True:
-    x = raw_input ()
-    time.sleep(1) # without this delay the test would fail about 75% of the time. Why?
-    print x.swapcase()
-    sys.stdout.flush()
