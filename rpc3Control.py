72afdee6292cd99e08681b3980387368abef232e
pponce <pponce@users.noreply.github.com>
2026-05-14 17:26:03 -0700

    Refactor rpc3Control.py for Python 3 compatibility

--- rpc3Control.py
+++ rpc3Control.py
@@ -1,7 +1,4 @@
-#!/usr/bin/python
-
-# Control Class for the Baytech RPC3
-# J. Adams <jna@retina.net>
+#!/usr/bin/env python3
 
 from pexpect import *
 import sys
@@ -9,216 +6,138 @@ import re
 import syslog
 import time
 import os
-import errno
-import datetime
 import fcntl
 
 class rpc3ControlError(Exception):
     def __init__(self, value):
         self.value = value
     def __str__(self):
-        return repr(self.value)
+        return str(self.value)
 
 class rpc3Control:
-    # Class to control a Baytech RPC-3
     child = None
     status = {}
     name = {}
     statuscached = False
-    unitid = ""
 
-    def __init__(self, hostname, user, password=None, debug=False):
+    def __init__(self, hostname, user=None, password=None, debug=False):
         self.hostname = hostname
         self.user = user
         self.password = password
         self.debug = debug
         self.connect()
-        self.unitid = ""
         self.child.delaybeforesend = .05
 
-      #  the RPC supports username, or password, or both
-      #  so we support both of those cases in this code.
+        # 1. Clear the initial banner noise
+        self.child.expect([".*Unit ID: (.*)", TIMEOUT], timeout=2)
+        
+        # 2. Wake up PDU
+        self.child.send("\r")
+        
+        # 3. Handle prompt
+        idx = self.child.expect(["Enter username>", "Enter Selection>", TIMEOUT], timeout=10)
 
-        # get unit id
-        check = self.child.expect([".*Unit ID: (.*)", EOF, TIMEOUT], timeout=3)
-        if check == 0:
-                self.unitid = self.child.match.group(1)
-        if check > 0:
-                if check == 1:
-                        raise rpc3ControlError("EOF during read")
-                else:
-                        raise rpc3ControlError("Timeout during read in init function")
-
-#       print(str(self.unitid))
-        if user != None:
-            self.es("Enter username>", user)
-
-        if password != None:
-            self.es("Enter password>", password)
-
-    def file_exists(self, path, filename):
-        for file_or_folder in os.listdir(path):
-                if file_or_folder == filename:
-                        return True
-        return False
-
-    def es(self,str_expect,str_send):
-
-       # a pexpect helper method; expect and send with error monitoring
-
-        result = self.child.expect([str_expect, EOF, TIMEOUT], timeout=3)
-#        print(str(self.child))
-
-        if result == 0:
-            self.child.send("%s\r" % str_send)
-            return
-
-        if result > 0:
-            if result == 1:
-                raise rpc3ControlError("EOF during read")
-            else:
-                raise rpc3ControlError("Timeout during read in ES function")
+        if idx == 0:
+            self.child.send(f"{user}\r")
+            p_idx = self.child.expect(["Enter password>", "Enter Selection>", TIMEOUT], timeout=5)
+            if p_idx == 0:
+                self.child.send(f"{password}\r")
+                self.child.expect("Enter Selection>", timeout=5)
+        elif idx == 2:
+            self.child.send("MENU\r")
+            self.child.expect("Enter Selection>", timeout=5)
 
     def connect(self):
-        if self.child == None:
-            self.child = spawn("telnet " + self.hostname)
-            result = self.child.expect(["Connected to", EOF, TIMEOUT], timeout=3)
-            #print (self.child)
-            #self.child.send("\r")
-
-        if self.debug == True:
+        if self.child is None:
+            # Added encoding here to handle string/byte conversion automatically
+            self.child = spawn(f"telnet {self.hostname}", encoding='utf-8', codec_errors='ignore')
+            self.child.expect(["Connected to", EOF, TIMEOUT], timeout=5)
+        if self.debug:
+            # In Python 3, if encoding is set in spawn, we use sys.stdout
             self.child.logfile = sys.stdout
 
-    def outlet(self,outlet_number,state):
-
-      #  control an outlet
-      #  state is one of on, off,or reboot
-      #  outlet_number is an integer.
-
+    def outlet(self, outlet_number, state):
         if state not in ("on", "off", "reboot"):
             self.child.send("6\r")
             self.child.terminate()
             raise rpc3ControlError('Invalid outlet state')
 
-        if int(outlet_number) > 8 or int(outlet_number) < 1:
+        if not (1 <= int(outlet_number) <= 8):
             self.child.send("6\r")
             self.child.terminate()
             return None
 
         self.es("Enter Selection>", "1")
-        self.es("RPC-3>", "%s %d\rY"  % (state, outlet_number) )
+        self.child.expect('Type "Help" for a list of commands', timeout=5)
+        self.es("RPC-3>", f"{state} {outlet_number}", timeout=10)
+        self.child.expect('Type "Help" for a list of commands', timeout=5)
         self.es("RPC-3>", "MENU")
-
+        
         self.statuscached = False
-        self.child.send("6\r")
-        self.child.terminate()
-
+        self.logout()
         return True
 
-    def outlet_status(self, outlet_number, ignore_cache = True):
-        # Get the status of an outlet
+    def es(self, str_expect, str_send, timeout=3):
+        result = self.child.expect([str_expect, EOF, TIMEOUT], timeout=timeout)
+        if result == 0:
+            self.child.send(f"{str_send}\r")
+            return
+        raise rpc3ControlError(f"Error waiting for '{str_expect}'")
 
-        if self.statuscached == True and ignore_cache != True:
-            if self.user == "admin" or self.user == None:
-                self.child.send("6\r")
-            else:
-                self.child.send("LOGOUT\r")
-            return (self.status[outlet_number],self.name[outlet_number])
+    def logout(self):
+        if self.child:
+            self.child.send("6\r")
+            try:
+                self.child.expect(EOF, timeout=2)
+            except:
+                pass
+            self.child.terminate()
+            self.child = None
 
-        if int(outlet_number) > 8 or int(outlet_number) < 1:
-            if self.user == "admin" or self.user == None:
-                self.child.send("6\r")
-            else:
-                self.child.send("LOGOUT\r")
-            return None
+    def outlet_status(self, outlet_number):
+        self.child.send("1\r")
+        idx = self.child.expect(['Type "Help" for a list of commands', TIMEOUT], timeout=10)
+        
+        if idx == 1:
+            return False
 
-        if self.user == "admin" or self.user == None:
-                self.es("Enter Selection>", "1")
-                self.es("RPC-3>", "MENU")
+        # full_text is already a string because of spawn encoding
+        full_text = self.child.before
+        
+        pattern = r'(\d+)\s+([A-Za-z0-9\-_]+)\s+(\d+)\s+(On|Off)'
+        found_any = False
+        
+        for line in full_text.splitlines():
+            match = re.search(pattern, line, re.IGNORECASE)
+            if match:
+                o_num = int(match.group(3))
+                self.status[o_num] = match.group(4).strip().lower() == "on"
+                self.name[o_num] = match.group(2).strip()
+                found_any = True
 
-        # parse the output
-        inlist = False
+        self.update_status_file(outlet_number)
+        self.child.send("\r") 
+        return self.status.get(outlet_number, False)
 
-        for line in self.child.before.split('\n'):
-            if line.rstrip() == "" and inlist:
-                inlist = False
+    def update_status_file(self, outlet_number):
+        lock_filename = '/var/lib/homebridge/rpc3control/telnetrunning.txt'
+        try:
+            with open(lock_filename, 'w') as frunning:
+                sttime = str(int(time.time() * 1000))
+                frunning.write(f"{sttime},idle,\n") 
+                for i in range(1, 9):
+                    s = self.status.get(i, "Unknown")
+                    n = self.name.get(i, "Unknown")
+                    frunning.write(f"{i},{s},{n}\n")
+        except Exception as e:
+            if self.debug: print(f"WRITE ERROR: {e}")
 
-        #without the below check i would get parse errors randomly when checking state
-            if line.rstrip() == "    5)...Reset Unit" and inlist:
-                inlist = False
-
-            if inlist:
-
-                m = re.match('^ .....([0-9]) ..... (...........) ([0-9]) ..... (On|Off)',line)
-
-                self.name[int(m.group(1))] = m.group(2).rstrip()
-
-                if m.group(4) == "On":
-                    self.status[int(m.group(1))] = True
-                else:
-                    self.status[int(m.group(1))] = False
-
-            if line.find("Status") != -1:
-                inlist = True
-
-        self.statuscached = True
-
-        if self.user == "admin" or self.user == None:
-                self.child.send("6\r")
-                self.child.terminate()
-        else:
-                self.child.send("LOGOUT\r")
-                self.child.terminate()
-
-        #Write all outlet status to file and lock the file. Doing this because i use this script with homebridge and siri executes the script to get status for each outloet. The RPC unit allows only 4 telnet sesions for user admin at a time. I have homebridge use the this status file instead of logging in if the status file is recent.
-        lock_filename = 'telnetrunning.txt'
-        frunning = open(lock_filename,'w+')
-        while True:
-                try:
-                        fcntl.flock(frunning, fcntl.LOCK_EX | fcntl.LOCK_NB)
-                        break
-                except IOError as e:
-                        # raise on unrelated IOErrors
-                        if e.errno != errno.EAGAIN:
-                                raise
-                        else:
-                                time.sleep(0.05)
-
-        i=1
-        sttime = str(int(round(time.time() * 1000)))
-        frunning.write(sttime + "," + str(outlet_number) + ",\r\n")
-        while  (i <= 8):
-                writeText = "%d," % (i)
-                stringOutletStatus = "%s," % self.status[i]
-                stringOutletName = "%s" % self.name[i]
-                frunning.write(writeText + stringOutletStatus + stringOutletName  + "\r\n")
-                i=i+1
-        fcntl.flock(frunning, fcntl.LOCK_UN)
-        frunning.close()
-        return (self.status[outlet_number],self.name[outlet_number])
-
-# fetch credentials
-# the credentials should be in a file called ".credentials" and in the form "hostname:user:pass" on one line.
 def load_credentials(credentials):
-    user = None
-    pw = None
-    rpc = None
-    err = None
-    whitelist = None
-
     try:
-        f = open(credentials, 'r')
-        (rpc, user, pw, w) = f.readline().rstrip().split(":",3)
-        whitelist = w.split(",")
-        f.close()
-    except IOError:
-        err = "FATAL: Couldn't open credentials file"
-    except ValueError:
-        err = "FATAL: Malformed %s file. Credentails should be in the form \"host:user:pw\"" % credentials
-
-    if err != None:
-        syslog.syslog(syslog.LOG_ERR, err)
-        print >> sys.stderr, err
+        with open(credentials, 'r') as f:
+            line = f.readline().rstrip()
+            parts = line.split(":", 3)
+            return (parts[0], parts[1], parts[2], parts[3].split(",") if len(parts) > 3 else [])
+    except Exception as e:
+        sys.stderr.write(f"FATAL: Credential error: {e}\n")
         sys.exit(1)
-
-    return (rpc,user,pw,whitelist)
